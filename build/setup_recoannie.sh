6f443516ce0fdcb3529fbcff20796ce21dc38d23
Steven Gardiner <sjgardiner@ucdavis.edu>
2017-10-03 01:08:44 -0700

    Add setup script to adjust the LD_LIBRARY_PATH and ROOT_INCLUDE_PATH environment variables for use with recoANNIE

---
+++ build/setup_recoannie.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get the path to the directory that holds this setup script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SCRIPT_DIR
+export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:$SCRIPT_DIR/../include
